44907fdedec404f0d99e062c98b63e8f08b66ec6
avedyer <avedyer@gmail.com>
2022-02-20 17:37:42 -0500

    click saving logic

--- src/components/card.js
+++ src/components/card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = (props) => {
     return (
-        <div className='card'>
+        <div className='card' onClick={props.click}>
             <img src={props.img} alt={props.name}/>
             <h3 className='name'>{props.name}</h3>
         </div>
--- src/components/table.js
+++ src/components/table.js
@@ -4,13 +4,41 @@ import Card from './card.js'
 const Table = (props) => {
 
     let characters = props.characters;
+    let clicked = []
+
+    function logClick (character) {
+        if(clicked.some(c => c.name === character.name)) {
+            clicked.length = 0;
+            console.log('BzZT')
+        }
+        else {
+            clicked.push(character);
+
+            if (clicked.length === characters.length) {
+                alert('You Win!');
+                reset();
+            }
+        }
+    }
+
+    function reset() {
+        clicked.length = 0;
+    }
 
 
     return (
         <div className='table'>
             {characters.map((character) => {
                 return (
-                    <Card name={character.name} img={character.img}/>
+                    <div className='sleeve' onClick={() => {
+                        console.log(character.name);
+                        logClick(character);
+                    }}>
+                        <Card 
+                        name={character.name} 
+                        img={character.img}
+                    />
+                    </div>
                 )
             })}
         </div>
