2a47114f15d1b5cf05f1990f15024a299a685171
avedyer <avedyer@gmail.com>
2022-02-22 09:04:11 -0500

    title card and mobile sizing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import characters from './components/characters.js';
 import Table from './components/table.js'
+import Header from './components/header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Table characters={characters}/>
     </div>
   );
---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import title from '../imgs/title.png'
+
+const Header = () => {
+    return (
+        <header>
+            <img src={title} alt='MEMORY TIME'/>
+        </header>
+    )
+}
+
+export default Header
